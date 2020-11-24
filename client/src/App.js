983705e7b5f78cf3650583c0cfd160467b3c5503
Roderick Trice <rocky_trice@yahoo.com>
2020-11-23 23:57:59 -0500

    setting up routing for login, register, and home page

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+
+
+const App = () => {
+  return (
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Login' component={Login}  />
+        <Route exact path='/Register'  component={Register}  />
+    </Switch>
+  );
+}
+
+export default App;
