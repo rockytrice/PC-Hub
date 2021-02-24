3c8c463ec33b0edcf89681feb88bd83884a0a4bf
Roderick Trice <rocky_trice@yahoo.com>
2021-02-24 00:06:50 -0500

    setting up redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
 import Login from './pages/auth/Login';
 import Home from './pages/Home';
 import Register from './pages/auth/Register';
