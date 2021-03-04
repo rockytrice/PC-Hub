e5bea91ebb372dcf446fb3a40b6de57e27dfa397
Roderick Trice <rocky_trice@yahoo.com>
2021-03-03 22:33:02 -0500

    logging in with google and conditionally rendering links in header with  logged in state and not logged in state

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -2,8 +2,9 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import firebase from 'firebase';
 import { Menu } from 'antd';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from "react-router-dom";
+
 import { HomeTwoTone, UserAddOutlined,UserOutlined,SettingOutlined, AppstoreOutlined,LogoutOutlined } from '@ant-design/icons';
 //destructure of menu.item
 const { SubMenu,Item } = Menu;
@@ -13,6 +14,8 @@ const { SubMenu,Item } = Menu;
 const Header = () =>{
     const [current, setCurrent]  = useState('home');
     let history = useHistory();
+//using the three dots spread out, you can spread out the states or whatever values you have.
+    let {user} = useSelector((state) =>({...state}));
     let dispatch = useDispatch();
 const handleClick = (e)=>{
     //console.log(e.key);
@@ -32,18 +35,26 @@ const logout = () =>{
             <Item key="home" icon={<HomeTwoTone />}>
                <Link to="/">Home</Link>
             </Item>
-            <Item key="register" icon={<UserAddOutlined />} className="float-right">
-               <Link to="/register">Register</Link>
-            </Item>
-            <Item key="login" icon={<UserOutlined />} className=' float-right'>
-                <Link to="/login">Login</Link>
-            </Item>
 
-            <SubMenu icon={<SettingOutlined />} title="Username">
+            {!user && (
+                <Item key="register" icon={<UserAddOutlined />} className="float-right">
+                    <Link to="/register">Register</Link>
+                </Item>
+            )}
+            {!user && (
+                <Item key="login" icon={<UserOutlined />} className=' float-right'>
+                    <Link to="/login">Login</Link>
+                </Item>
+            )}
+            {user && (
+                <SubMenu icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]} className="float-right">
                     <Item key="setting:1">Option 1</Item>
                     <Item key="setting:2">Option 2</Item>
-                <Item icon={<LogoutOutlined />}  onClick={logout}>Logout</Item>
-            </SubMenu>
+                    <Item icon={<LogoutOutlined />}  onClick={logout}>Logout</Item>
+                </SubMenu>
+            )}
+
+
         </Menu>
     )
 }
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import {auth} from '../../firebase';
+import {auth, googleAuthProvider} from '../../firebase';
 import {toast} from 'react-toastify';
 import {useDispatch} from 'react-redux';
 
 import {Button} from 'antd';
-import { MailOutlined } from '@ant-design/icons';
+import { MailOutlined , LoadingOutlined, GoogleOutlined } from '@ant-design/icons';
 
 const Login = ({history}) => {
     //create state to store user's email
@@ -36,6 +36,26 @@ const Login = ({history}) => {
             setLoading(false)
         }
     };
+    const googleLogin = async ()=>{
+    auth.signInWithPopup(googleAuthProvider)
+        .then(async (result)=>{
+            const {user} = result
+            const idTokenResult = await user.getIdTokenResult();
+            dispatch({
+                type: 'LOGGED_IN_USER',
+                payload: {
+                    email: user.email,
+                    token: idTokenResult.token,
+                },
+            });
+            history.push('/')
+        })
+        .catch((error)=>{
+            console.log(error)
+            toast.error(error.message)
+        })
+
+    }
     const loginForm = ()=> (
         <form onSubmit={handleSubmit}>
             <div className="form-group">
@@ -49,25 +69,39 @@ const Login = ({history}) => {
                        placeholder="Enter your password"
                        onChange={event => setPassword(event.target.value)}/>
             </div>
+            <div className="mt-4">
+                <Button
+                    onClick={handleSubmit}
+                    type="primary"
+                    block
+                    shape="round"
+                    icon={<MailOutlined />}
+                    size="large"
+                    disabled={!email || password.length < 6}
+                    className="mb-3">
+                    Login with Email/Password
+                </Button>
+            </div>
+           <div>
+               <Button
+                   onClick={googleLogin}
+                   type="danger"
+                   block
+                   shape="round"
+                   icon={<GoogleOutlined />}
+                   size="large"
+                   className="mb-3">
+                   Login with Google
+               </Button>
+           </div>
 
-            <Button
-                onClick={handleSubmit}
-                type="primary"
-                block
-                shape="round"
-                icon={<MailOutlined />}
-                size="large"
-                disabled={!email || password.length < 6}
-                className="mb-3">
-                Login with Email/Password
-            </Button>
         </form>
     );
     return (
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4 className="b-0 my-2">Login</h4>
+                    {loading ? <LoadingOutlined /> : <h4>Login</h4>}
                     {loginForm()}
                 </div>
             </div>
